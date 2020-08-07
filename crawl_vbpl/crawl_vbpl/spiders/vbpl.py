5b12d29073e85d6048810ab0cb02a4958d283d0d
Do Ly <kieulydt@gmail.com>
2020-08-07 18:57:07 +0700

    change spider 1

--- crawl_vbpl/crawl_vbpl/spiders/vbpl.py
+++ crawl_vbpl/crawl_vbpl/spiders/vbpl.py
@@ -6,28 +6,35 @@ cookie = {
         "Culture":"vi",
         "G_ENABLED_IDPS":"google",
         "__zlcmid":"zYjL3SrShSvU6C",
-        "thuvienphapluatnew":"E90C322C9F718303F23B0BC2198073735BF338B7E3BBC1E78D16CB4DD4EA29BA4E8650EAFBBFF5A28126B73AF03D559B78D855DA8C87DE184B786E2EE09A0B43CD6D768F6863D3D9490E2CD831B4AD92C037BBF3ACF9C8611D6DCC7964D767F6696F8378C7ECB2BC4ACD12EBEEA1E091D81432710973B6049790DF10555CB37853763B4C3E44F638C1D2377B66906727C39EB3EF197D967E7B7C326517F5F3DBE4AC36F137FD630CFDFFFB1DC5B3B495E71085B4",
+        "thuvienphapluatnew":"E5357374601F2272ABF5168549C05F4E25CF95EE68208D614D12BFD5B7ECF837814DB70CD5922E71C131E7675C0998BD211767ADA0F223725FF2FC0DDC9F34018FC7DE826C6BD09B60B3C9B3A64871B7576ED14625F3B81429EFC0B1128F109A133BE124A7809D5A57594CC38FF16866D03725AD7E90D8CB3F5BD3DBA76E78190631C1CAEE38271A6E8D5CA791B83D52ECD2F8C9776F9162A87D370D340FE985E7D3273257A32D3749326506AA8D9589DC9F9701",
         "dl_user":"U91GdVpHRjFkSFWW",
         "ASP.NET_SessionId":"kbp4vwd3powhquuhxmdpvfdt",
         "Cookie_VB":"close",
-        "__utma":"19472893.943406357.1596771235.1596771235.1596771235.1",
-        "__utmc":"19472893",
-        "__utmz":"19472893.1596771235.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)",
+        "__utma":"173276988.268027361.1596798415.1596798415.1596798415.1",
+        "__utmc":"173276988",
+        "__utmz":"173276988.1596798415.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)",
         "__utmt":"1",
-        "__utmb":"19472893.3.8.1596771235"
+        "__utmb":"173276988.4.9.1596798427561"
     }
 
 
 script2 = """
 function main(splash)
-    assert(splash:go(url))
-    assert(splash:wait(0.5))
-    urls = splash:select('a#Content_ThongTinVB_vietnameseHyperLink')
-        urls:click()
-    assert(splash:wait(3))
-    return {
-        
-        html = splash:html()
+    assert(splash:go(args.url))
+      assert(splash:wait(6))
+      username = splash:select('input#Content_ThongTinVB_Support_usernameTextBox')
+      password = splash:select('input#Content_ThongTinVB_Support_passwordTextBox')
+      username:send_text("bandautu")
+      password:send_text("bandautu")
+      login_button = splash:select('input#Content_ThongTinVB_Support_loginButton')
+      assert(splash:wait(3))
+      login_button:click()
+      assert(splash:wait(4))
+      tai_ve_button = splash:select('span#Content_ThongTinVB_SpDoanload')
+  	  tai_ve_button:click()
+      assert(splash:wait(3))
+      return {
+        html = splash:html(),
     }
 end
 """
@@ -49,14 +56,12 @@ class VbplSpider(scrapy.Spider):
     def parse_item(self, response):
         # print(response.url)
         x = response.css('div.content-0 div.nq p.links-bot a::attr(href)').extract()
-        # print(x)
         urls = []
         for item in x:
             if(item.find('?tab=7')!=-1):
                 urls.append(item)
         for item in urls:
-            # requests.meta['url'] = item
-            yield SplashRequest(url=item, cookies=cookie, callback=self.parse_file, args={'lua_source':script2})
+            yield SplashRequest(url=item, cookies=cookie,callback=self.parse_file, args={'lua_source': script2})
 
     def parse_file(self, response):
 
